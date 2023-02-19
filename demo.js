7a48a6dd1e1372577ed045e96e71bbd0e03a9fe4
anurag530 <anurag.shukla530@gmail.com>
2023-02-20 01:07:33 +0530

    task3complete

--- demo.js
+++ demo.js
@@ -1,1 +1,9 @@
-console.log('i love git')
+//console.log('i love git')
+var headerTitle=document.getElementById('header-title');
+var header=document.getElementById('main-header');
+//console.log(headerTitle);
+//headerTitle.textContent='hello';
+//headerTitle.innerText='goodbye';
+//console.log(headerTitle.innerText);
+header.style.borderBottom='solid 3px #000';
+document.getElementById("title").style.color = "green";
